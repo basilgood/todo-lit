27370fae2549db721b243a73dc1e4aa8865bd153
Luta Vasile <elsile69@yahoo.com>
2022-04-11 10:18:53 +0300

    feat: toggle completed

--- src/js/main.js
+++ src/js/main.js
@@ -1,40 +1,52 @@
 import { component, useEffect, useState } from 'haunted';
 import { html } from 'lit-html';
-import './../styles/main.css';
+import '../styles/main.css';
 
 const TodoList = (host) => {
   const [todos, setTodos] = useState([]);
-  const submit = (event) => event.preventDefault();
+  const [done, setDone] = useState('');
+  const todoItem = {
+    text: '',
+    done: false,
+    active: false,
+    total: 0,
+  };
   const list = todos.map(
     (todo, index) =>
-      html` <li class="todo-item" title="">
-        <button
-          @click=${() => {
-            const done = todos[index];
-            setTodos(
-              todos.map((todo) =>
-                todo == todos[index] ? todo + ' (done)' : todo
-              )
-            );
+      html` <input
+          type="checkbox"
+          id="done"
+          @change=${() => {
+            const checkbox = host.shadowRoot.querySelector('#done');
+            setDone(() => (checkbox.checked ? 'done' : ''));
           }}
-        >
-          ✔
-        </button>
-        ${todo}
-        <button
-          @click=${() => {
-            setTodos(todos.slice(0, index).concat(todos.slice(index + 1)));
-          }}
-        >
-          x
-        </button>
-      </li>`
+        />
+        <li class=${done + ' todo-item'}>
+          ${todo}
+          <button
+            @click=${() => {
+              setTodos(todos.slice(0, index).concat(todos.slice(index + 1)));
+            }}
+          >
+            x
+          </button>
+        </li>`
   );
   useEffect(() => {
     console.log(todos);
   }, [todos]);
   return html`
     <style>
+      .todo {
+        display: grid;
+        align-content: space-evenly;
+        justify-content: space-evenly;
+      }
+      .todo-input {
+        background: rgba(0, 0, 0, 0.003);
+        box-shadow: inset 0 -2px 1px rgba(0, 0, 0, 0.03);
+        min-width: 550px;
+      }
       .todo-list {
         list-style-type: none;
         padding: 0;
@@ -45,14 +57,32 @@ const TodoList = (host) => {
         background: #cccccc;
         border-bottom: 1px solid #ffffff;
       }
+      .todo-filter li {
+        display: inline;
+      }
+      .todo-bar {
+        color: #777;
+        padding: 10px 15px;
+        height: 20px;
+        text-align: center;
+        border-top: 1px solid #e5e5e5;
+      }
+      #done {
+        float: left;
+        padding: 0 10px 0 0;
+      }
+      .done {
+        color: red;
+      }
     </style>
-    <h3>To Do List</h3>
-    <form @submit=${submit} class="todo-form">
+    <div class="todo">
+      <h3>To Do List</h3>
       <input
         type="text"
         placeholder="Add a todo"
         class="todo-input"
         value=${todos}
+        autofocus
       />
       <button
         class="todo-button"
@@ -63,10 +93,17 @@ const TodoList = (host) => {
       >
         Add todo
       </button>
-    </form>
-    <ul class="todo-list">
-      ${list}
-    </ul>
+      <ul class="todo-list">
+        ${list}
+      </ul>
+      <div class="todo-bar">
+        <ul class="todo-filter">
+          <li>items</li>
+          <li>Active</li>
+          <li>Completed</li>
+        </ul>
+      </div>
+    </div>
   `;
 };
 
